c9fce7f055ac737796cc5d172f213df29c860122
SebastianSamayoa <samayoa_jhoan@hotmail.com>
2018-09-25 20:36:23 -0600

    creacion del registro y del login

---
+++ src/app/registered/registered.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registered',
+  templateUrl: './registered.component.html',
+  styles: []
+})
+export class RegisteredComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
