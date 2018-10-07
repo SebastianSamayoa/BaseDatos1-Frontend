9bc7d859dad348eb1c9205d2b20308f2f8a5d2ac
SebastianSamayoa <samayoa_jhoan@hotmail.com>
2018-10-07 12:13:55 -0600

    creacion de los primeros charts para del dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { PersonaService } from '../../services/persona.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,12 +9,17 @@ import { PersonaService } from '../../services/persona.service';
 })
 export class DashboardComponent implements OnInit {
 
+  chartbar: any;
+  chartpie: any;
+
   constructor(
     private _persona: PersonaService
   ) { }
 
   ngOnInit() {
     this.cargarPersona();
+    this.cargandobars();
+    this.cargandopie();
   }
 
   cargarPersona() {
@@ -27,4 +33,67 @@ export class DashboardComponent implements OnInit {
     );
   }
 
+  cargandobars() {
+    this.chartbar = new Chart('canvas', {
+      type: 'bar',
+      data: {
+          labels: ['Produccion', 'Ventas', 'Despacho', 'Clientes Satisfechos', 'Reclamos', 'Inventario'],
+          datasets: [{
+              label: 'Empresa de Hilos',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
+  }
+
+  cargandopie() {
+    this.chartpie = new Chart( 'pie', {
+      type: 'doughnut',
+    data: {
+      labels: ['Sin Entrega', 'Entregados', 'En Camino'],
+      datasets: [{
+          data: [5, 20, 30],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)'],
+            borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)']
+        }]},
+      options: {
+        title: {
+          display: true,
+          text: 'Venta de Productos'
+        }
+      }
+    });
+  }
 }
